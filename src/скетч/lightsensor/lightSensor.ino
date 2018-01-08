d9cdcb944c44b110b5fdea96ec0a9f21d3be9eca
SashaBondar <albon10101983@gmail.com>
2018-01-08 16:46:55 +0200

    добавляем мигание лампочек

--- src/скетч/lightsensor/lightSensor.ino
+++ src/скетч/lightsensor/lightSensor.ino
@@ -1,18 +1,30 @@
 const int MIN_LUM = 1;
+const int MAX_LUM = 3;
 const int pinPhoto = A0;
+const int pinLamp = 5;
 int raw = 0;
+int lamp = 0;
 
 void setup() {
   Serial.begin(9600);
   pinMode( pinPhoto, INPUT );
+	pinMode( pinLamp, INPUT );
 }
 
 void loop() {
   raw = analogRead( pinPhoto );
     if ( raw <= MIN_LUM )
     {
-	// включить лампу
+	// ???????? ?????
+			digitalWrite(5, HIGH);
+			delay(1000);
     }
+		else if ( raw > MAX_LUM )
+		{
+	// ????????? ?????
+			digitalWrite(5, LOW);
+			deplay(1000);
+		}
   Serial.println( raw );
   delay(200);
 }
